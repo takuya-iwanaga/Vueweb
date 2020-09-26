69f1501bb0031dadc560a751b2a56d4ae6e8bd83
takuya-iwanaga <nishi.iwa0901@gmail.com>
2020-09-26 12:58:34 +0900

    はじめてのVue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="ganchanの部屋 "/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  background: url("assets/back.jpg");
+  background-size: cover;
+  background-position: center;
+  width: auto;
+  height: auto;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
